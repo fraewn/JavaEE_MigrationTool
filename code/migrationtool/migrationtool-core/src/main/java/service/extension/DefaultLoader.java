5f3d5bf34d90febfd2cf3049b42c0501f323fb10
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 18:15:02 +0200

    Load enums

--- code/migrationtool/migrationtool-core/src/main/java/service/extension/DefaultLoader.java
+++ code/migrationtool/migrationtool-core/src/main/java/service/extension/DefaultLoader.java
@@ -8,7 +8,6 @@ import org.apache.log4j.Logger;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.ConstructorDeclaration;
-import com.github.javaparser.ast.body.EnumConstantDeclaration;
 import com.github.javaparser.ast.body.EnumDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
@@ -40,82 +39,85 @@ public class DefaultLoader extends LoaderService<Object, List<ClassDTO>> {
 		units.forEach(unit -> {
 			// Find all classes in file
 			List<ClassOrInterfaceDeclaration> classList = unit.findAll(ClassOrInterfaceDeclaration.class);
-			List<EnumConstantDeclaration> enumList = unit.findAll(EnumConstantDeclaration.class);
-			// if the file has no classes, skip further work
-			if (classList.isEmpty()) {
-				return;
-			}
-			
-			for(EnumConstantDeclaration decl : enumList){
-				
-				ClassDTO classDTO = new ClassDTO();
-				//String classPath = decl.getFullyQualifiedName().get().toString();
-				LOG.debug("Inspect class: " + "EEEEEEEENUM: " + decl.getNameAsString());
-				//classDTO.setFullName(classPath);
-				System.out.println("Enum: " + classDTO.getFullName());
-				// read package and module declaration from unit (same for all classes in unit)
-				unit.getPackageDeclaration().ifPresent(v -> classDTO.setPackageDeclaration(v.getNameAsString()));
-				unit.getModule().ifPresent(v -> classDTO.setModuleDeclaration(v.getNameAsString()));
-				// save imports
-				classDTO.setImports(unit.getImports());
-				// save class
-				//classDTO.setEnumDecl(decl);
-				// add interfaces the class implements
-				//classDTO.setImplementations(decl.getImplementedTypes());
-				// add classes the class extends from
-				classDTO.setExtensions(null);
-				// add fields of class
-				classDTO.setFields(decl.findAll(FieldDeclaration.class));
-				// add constructors of class
-				classDTO.setConstructors(decl.findAll(ConstructorDeclaration.class));
-				// add methods of class
-				classDTO.setMethods(decl.findAll(MethodDeclaration.class));
-				// collect all annotations for a class
-				classDTO.setAnnotationDeclarationList(decl.getAnnotations());
-				// collect all type parameters
-				classDTO.setTypeParameters(null);
-				// add dto to the collection
-				this.classes.add(classDTO);
-			}
-			
-			
 			// iterate all classes in file
 			for (ClassOrInterfaceDeclaration decl : classList) {
 				String classPath = decl.getFullyQualifiedName().get().toString();
-				LOG.debug("Inspect class: " + classPath);
 				// ignore the class if it is nested (inner class)
 				if (decl.isInnerClass()) {
 					LOG.debug("Ignoring class " + classPath + " since it's nested");
 					continue;
 				}
+				LOG.debug("Inspect class: " + decl.getNameAsString());
 				ClassDTO classDTO = new ClassDTO();
 				classDTO.setFullName(classPath);
-				// read package and module declaration from unit (same for all classes in unit)
-				unit.getPackageDeclaration().ifPresent(v -> classDTO.setPackageDeclaration(v.getNameAsString()));
-				unit.getModule().ifPresent(v -> classDTO.setModuleDeclaration(v.getNameAsString()));
 				// save imports
 				classDTO.setImports(unit.getImports());
-				// save class
-				classDTO.setJavaClass(decl);
-				// add interfaces the class implements
-				classDTO.setImplementations(decl.getImplementedTypes());
-				// add classes the class extends from
-				classDTO.setExtensions(decl.getExtendedTypes());
-				// add fields of class
-				classDTO.setFields(decl.findAll(FieldDeclaration.class));
-				// add constructors of class
-				classDTO.setConstructors(decl.findAll(ConstructorDeclaration.class));
-				// add methods of class
-				classDTO.setMethods(decl.findAll(MethodDeclaration.class));
-				// collect all annotations for a class
-				classDTO.setAnnotationDeclarationList(decl.getAnnotations());
-				// collect all type parameters
-				classDTO.setTypeParameters(decl.getTypeParameters());
+				// read package and module declaration from unit
+				unit.getPackageDeclaration().ifPresent(v -> classDTO.setPackageDeclaration(v.getNameAsString()));
+				unit.getModule().ifPresent(v -> classDTO.setModuleDeclaration(v.getNameAsString()));
 				// add dto to the collection
-				this.classes.add(classDTO);
+				this.classes.add(setClass(classDTO, decl));
+			}
+			// Find all enums in file
+			List<EnumDeclaration> enumList = unit.findAll(EnumDeclaration.class);
+			// if the file has no classes, skip further work
+			for (EnumDeclaration decl : enumList) {
+				String classPath = decl.getFullyQualifiedName().get().toString();
+				// ignore the class if it is nested (inner class)
+				if (decl.isNestedType()) {
+					LOG.debug("Ignoring enum " + classPath + " since it's nested");
+					continue;
+				}
+				LOG.debug("Inspect enum: " + decl.getNameAsString());
+				ClassDTO classDTO = new ClassDTO();
+				classDTO.setFullName(classPath);
+				// save imports
+				classDTO.setImports(unit.getImports());
+				// read package and module declaration from unit
+				unit.getPackageDeclaration().ifPresent(v -> classDTO.setPackageDeclaration(v.getNameAsString()));
+				unit.getModule().ifPresent(v -> classDTO.setModuleDeclaration(v.getNameAsString()));
+				// add dto to the collection
+				this.classes.add(setEnum(classDTO, decl));
 			}
 		});
 		return this.classes;
 	}
 
+	private ClassDTO setClass(ClassDTO classDTO, ClassOrInterfaceDeclaration decl) {
+		// save class
+		classDTO.setJavaClass(decl);
+		// add interfaces the class implements
+		classDTO.setImplementations(decl.getImplementedTypes());
+		// add classes the class extends from
+		classDTO.setExtensions(decl.getExtendedTypes());
+		// add fields of class
+		classDTO.setFields(decl.findAll(FieldDeclaration.class));
+		// add constructors of class
+		classDTO.setConstructors(decl.findAll(ConstructorDeclaration.class));
+		// add methods of class
+		classDTO.setMethods(decl.findAll(MethodDeclaration.class));
+		// collect all annotations for a class
+		classDTO.setAnnotationDeclarationList(decl.getAnnotations());
+		// collect all type parameters
+		classDTO.setTypeParameters(decl.getTypeParameters());
+		// collect all enums
+		classDTO.setEnums(decl.findAll(EnumDeclaration.class));
+		return classDTO;
+	}
+
+	private ClassDTO setEnum(ClassDTO classDTO, EnumDeclaration decl) {
+		// save class
+		classDTO.setEnumClass(decl);
+		// add interfaces the class implements
+		classDTO.setImplementations(decl.getImplementedTypes());
+		// add fields of class
+		classDTO.setFields(decl.findAll(FieldDeclaration.class));
+		// add constructors of class
+		classDTO.setConstructors(decl.findAll(ConstructorDeclaration.class));
+		// add methods of class
+		classDTO.setMethods(decl.findAll(MethodDeclaration.class));
+		// collect all annotations for a class
+		classDTO.setAnnotationDeclarationList(decl.getAnnotations());
+		return classDTO;
+	}
 }
